c157702a9d118574984c4070b727d6542d8603d6
Elizabeth Kovalchick <ekovalchick97@gmail.com>
2023-11-06 21:54:16 -0500

    update file orginization

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const profileRoutes = require('./profileRoutes');
 
-router.use('/',homeRoutes);
+
+router.use('/', homeRoutes);
+
+router.use('/profile', profileRoutes);
+
 router.use('/api', apiRoutes);
-//modularizing api routes
 
 module.exports = router;
